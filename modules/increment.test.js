11f4712beb9d3fdd07bdc7862ee112a23f2bbf8b
Daniel Sellers <daniel@designfrontier.net>
2019-09-19 22:39:05 -0600

    updated deps and fixed tests

--- modules/increment.test.js
+++ modules/increment.test.js
@@ -11,10 +11,10 @@ test('should increment only patch when incrementing patch', (t) => {
   t.is(increment('1.2.3', 'patch'), '1.2.4');
 });
 
-test('should increment only patch when incrementing patch', (t) => {
+test('should increment correctly for minor', (t) => {
   t.is(increment('1.2.3', 'minor'), '1.3.0');
 });
 
-test('should increment only patch when incrementing patch', (t) => {
+test('should increment correctly for major', (t) => {
   t.is(increment('1.2.3', 'major'), '2.0.0');
 });
